93ba5ff63f1b9f73e903243003e9c76bc4f6e400
tvsxar <tarasofficial82@gmail.com>
2026-06-05 17:28:37 +0300

    feat: add projects data file & projects screenshots

---
+++ src/data/projects.ts
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Mini Blog",
+    description:
+      "A simple blogging platform built with Next.js and MongoDB, allowing users to create, edit, and share their thoughts in a clean and intuitive interface.",
+    image: "/Blog.png",
+    link: "https://mini-blog-backend-6rg8.onrender.com/",
+    code: "https://github.com/tvsxar/mini-blog",
+    techs: [
+      "React",
+      "Tailwind",
+      "MongoDB",
+      "Express",
+      "Node.js",
+      "Docker",
+      "REST APIs",
+    ],
+  },
+  {
+    id: 2,
+    title: "Feedback Wall (Flowly)",
+    description:
+      "A feedback collection tool designed to help businesses gather and analyze customer feedback effectively, providing insights to improve products and services.",
+    image: "/Feedback.png",
+    link: "https://feedback-wall-next.onrender.com/",
+    code: "https://github.com/tvsxar/feedback-wall",
+    techs: ["Next.js", "Tailwind", "MongoDB", "REST APIs", "Docker"],
+  },
+  {
+    id: 3,
+    title: "Expense Flow (Spendly)",
+    description:
+      "An expense tracking application that allows users to manage their finances by categorizing expenses, setting budgets, and visualizing spending patterns through interactive charts.",
+    image: "/Expense.png",
+    link: "https://expense-flow-backend-8m66.onrender.com/",
+    code: "https://github.com/tvsxar/expense-flow",
+    techs: [
+      "React",
+      "TypeScript",
+      "Express",
+      "GraphQL",
+      "Chart.js",
+      "Node.js",
+      "Tailwind",
+      "MongoDB",
+      "Docker",
+    ],
+  },
+];
