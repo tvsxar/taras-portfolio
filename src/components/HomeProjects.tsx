3bc384e1cc81d013dae02a31127f5d25717ec48c
tvsxar <tarasofficial82@gmail.com>
2026-06-07 13:14:19 +0300

    fix: project cards and buttons

--- src/components/HomeProjects.tsx
+++ src/components/HomeProjects.tsx
@@ -30,7 +30,7 @@ function HomeProjects() {
                 </div>
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 mt-8 sm:mt-10 justify-items-center">
-                    {projects.map((project) => (
+                    {projects.slice(0, 3).map((project) => (
                         <ProjectCard key={project.id} project={project} />
                     ))}
                 </div>
